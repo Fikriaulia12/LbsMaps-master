cd5dadd48f8cca9523f0a0a627938f9fa6399c44
Fikri A.N <itachiuciha189@gmail.com>
2024-05-31 23:04:50 +0700

    Initial commit

---
+++ app/src/main/java/com/Fikri/lbsmaps/ActivityMapsBinding.java
@@ -0,0 +1,13 @@
+package com.Fikri.lbsmaps;
+
+import android.view.LayoutInflater;
+
+public class ActivityMapsBinding {
+    public static ActivityMapsBinding inflate(LayoutInflater layoutInflater) {
+        return null;
+    }
+
+    public int getRoot() {
+        return 0;
+    }
+}
